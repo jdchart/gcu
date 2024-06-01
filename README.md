22e1d79bce25a7cf7c9e1d5eb4c8d15c95c6d2a5
Jacob Hart <>
2024-06-01 20:13:59 +0200

    package setup

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # gcu
 A collection of functions I use to make my Google Colab notebooks cleaner.
+
+## Setup
+To use this package in a Colab notebook, run the following code in a cell:
+
+```python
+!pip install -q git+https://github.com/jdchart/gcu.git
+import gcu
+```
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from setuptools import find_packages
+
+long_description= """
+# gcu
+A collection of functions I use to make my Google Colab notebooks cleaner.
+"""
+
+required = []
+
+setup(
+    name="gcu",
+    version="0.0.1",
+    description="A collection of functions I use to make my Google Colab notebooks cleaner.",
+    long_description=long_description,
+    author="Jacob Hart",
+    author_email="jacob.dchart@gmail.com",
+    url="https://github.com/jdchart/gcu",
+    install_requires=required,
+    packages=find_packages()
+)
