0315ba76bcfab172312df1e0ebdcb2d4a602e9b5
Jacob Hart <>
2024-06-02 08:44:56 -0400

    remvoe condition

--- gcu/files/file.py
+++ gcu/files/file.py
@@ -60,10 +60,9 @@ def download(url, path = "", **kwargs) -> Union[File, List[File]]:
     downloaded = []
     if isinstance(url, str):
         url = [url]
-    else:
-        for item in url:
-            _download_online_file(url, os.path.join("/content", os.path.basename(item)), kwargs.get("range", None))
-            downloaded.append(os.path.join("/content", os.path.basename(item)))
+    for item in url:
+        _download_online_file(url, os.path.join("/content", os.path.basename(item)), kwargs.get("range", None))
+        downloaded.append(os.path.join("/content", os.path.basename(item)))
 
     if len(downloaded) > 0:
         return _process_media_get(path, downloaded, kwargs.get("new_filename", None))
