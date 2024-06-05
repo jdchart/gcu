3642b5ae738af0e1926d387e4e9f88b16f265182
Jacob Hart <>
2024-06-05 11:17:25 -0700

    read text files

--- gcu/files/text_files.py
+++ gcu/files/text_files.py
@@ -1,9 +1,17 @@
+import csv
+
 def read_plain_text(path):
     """Return plain text file as string."""
 
-    return None
+    with open(path, 'r') as f:
+        return f.read()
 
-def read_csv(path):
+def read_csv(path, **kwargs):
     """Return csv file as multi dimensional list."""
 
-    return None
+    content = []
+    with open(path, 'r') as f:
+        csv_reader = csv.reader(f, delimiter = kwargs.get("delimiter", ","), quotechar = kwargs.get("quotechar", '"'))
+        for row in csv_reader:
+            content.append(row)
+    return content
